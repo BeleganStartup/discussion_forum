40ddce8ff8eb3bbcb450254b9b50266e51a544b9
Boularaoui Okba <okba2fb@gmail.com>
2021-10-25 14:25:13 +0300

    initilze the project structure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # discussion_forum
+
 Predict Systems Interview
+
+**WARNING: .env file should never pushed to a git repository, but we do it in this case for testing purpose.**
+
+`1/ python manage.py makemigrations`
+
+`2/ python manage.py migrate`
+
+`3/ python manage.py createsuperuser`
+
+`4/ python manage.py runserver`
---
+++ source/urls.py
@@ -0,0 +1,28 @@
+"""source URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# Python Lib
+from decouple import config
+# Django
+from django.contrib import admin
+from django.urls import path, include
+
+ADMIN_DASHBOARD = config('ADMIN_DASHBOARD')
+BROWSABLE_API = config('ADMIN_DASHBOARD')
+
+urlpatterns = [
+    path(f'{ADMIN_DASHBOARD}/', admin.site.urls),
+    path(f'{BROWSABLE_API}/', include('rest_framework.urls'))
+]
